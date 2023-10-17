8945b263bac00da5763de3b217e1592d7f276fe4
DInesh Kumar <dinesh.kumar@lumanorth.com>
2023-10-17 16:50:38 +0100

    route?

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ async function cache(model) {
         for (let item of items) {
             let data = (await axios.get(`/pages/page/${item._id}`, { params: { populate: 1 } })).data
             data.route = data._routes.default
-            const path = data._routes.default.substring(1).replaceAll('/', '---')
+            const path = data._routes.default.substring(1).replaceAll('/', '---').replaceAll('#', '')
             await recursiveDownloadImages(data, { name: 'page', config: { } })
             fs.writeFileSync(`src/content/pages/${path}.json`, JSON.stringify(data, null, 2))
         }
